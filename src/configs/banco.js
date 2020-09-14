84217c1e16f4efd6bf5bd3f1cc78eef9e78d5d83
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-09-14 00:05:01 -0300

    Fix database errors

--- src/configs/banco.js
+++ src/configs/banco.js
@@ -1,57 +1,12 @@
-const sqlite = require('sqlite3').verbose();
-const banco = new sqlite.Database('./src/configs/todo_db.db'); 
+const sqlite = require("sqlite3").verbose();
 
-process.on('SIGINT', () =>
-    bd.close(() => {
-      console.log('Banco encerrado');
-      process.exit(0);
-    }) 
+const banco = new sqlite.Database("./database.db", sqlite.OPEN_READWRITE);
+
+process.on("SIGINT", () =>
+  banco.close(() => {
+    console.log("Banco encerrado");
+    process.exit(0);
+  })
 );
 
 module.exports = banco;
-
-/* dbDados.serialize(() => {
-  dbDados.run('INSERT INTO usuario (id_usuario, nome, senha, email) VALUES (01, "Samantha", 123456, "sassacardosoo@gmail.com")', (err) => {
-    if(err) {
-      console.log(err.message)
-    }
-  });
-    dbDados.each(`SELECT * FROM usuario`, (err, row) => {
-        if (err) {
-        console.error(err.message);
-        }
-        console.log(row.id_usuario + "\t" + row.nome + "\t" + row.senha + "\t" + row.email); 
-    });
-});
-
-dbDados.serialize(() => {
-    dbDados.run('INSERT INTO tarefas (id_tarefas, status, titulo, descricao) VALUES (01, "Do", "Arrumar casa", "Limpar casa toda semana")', (err) => {
-          if(err) {
-          console.log(err.message)
-          }
-      });
-    dbDados.each(`SELECT * FROM tarefas`, (err, row) => {
-          if (err) {
-            console.error(err.message);
-          }
-          console.log(row.id_tarefas + "\t" + row.status + "\t" + row.titulo + "\t" + row.descricao);
-        });
-  });
-
-dbDados.close((err) => {
-    if (err) {
-      console.error('Ocorreu um erro');
-    }
-    console.log('Fechando o banco de dados');
-  });
-
-module.exports = (app) => {
-    app.get('/:nome', (req, res) => {
-        for(let i=0; i<dadosDb.length; i++) {                   //banco de dados
-            if(req.params.nome == dbDados[i].nome) {            
-                res.send(geraPaginaTodo(dbDados[i].tarefas))
-            }
-        }
-    });
-
-} */
